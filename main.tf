2a4881e9cc558002346bebfed8d4574ce21806e2
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-20 14:43:55 +0200

    Added public_subnet_tags

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
 # VPC
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+	source = "terraform-aws-modules/vpc/aws"
 
-  name = "jenkins-vpc"
-  cidr = var.vpc_cidr
+	name = "jenkins-vpc"
+	cidr = var.vpc_cidr
 
-  azs            = data.aws_availability_zones.azs.names
-  public_subnets = var.public_subnets
+	azs            = data.aws_availability_zones.azs.names
+	public_subnets = var.public_subnets
 
-  enable_dns_hostnames = true
+	enable_dns_hostnames = true
 
-  tags = {
-    Name        = "jenkins-vpc"
-    Terraform   = "true"
-    Environment = "dev"
-  }
+	tags = {
+		Name        = "jenkins-vpc"
+		Terraform   = "true"
+		Environment = "dev"
+	}
+
+	public_subnet_tags = {
+		Name = "jenkins-subnet"
+	}
 }
 
 # SG
