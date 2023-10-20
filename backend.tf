3bb4a4ab7bed4aea4c4e510b96707de3f34d6c9b
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-20 14:31:11 +0200

    Added base tf files and configured tf backend

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+	backend "s3" {
+		# NOTE: a bucket with this exact name must be already created on AWS
+		bucket = "terraform-kubernetes-jenkins-eks"
+		key    = "jenkins/terraform.tfstate"
+		region = "eu-central-1"
+	}
+}
