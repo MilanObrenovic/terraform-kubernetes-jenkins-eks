b962d4d232ec7928e61b29fdf568e8ccdc80b44a
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-20 14:49:09 +0200

    Configured security group

--- main.tf
+++ main.tf
@@ -1,26 +1,63 @@
 # VPC
 module "vpc" {
-	source = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
-	name = "jenkins-vpc"
-	cidr = var.vpc_cidr
+  name = "jenkins-vpc"
+  cidr = var.vpc_cidr
 
-	azs            = data.aws_availability_zones.azs.names
-	public_subnets = var.public_subnets
+  azs            = data.aws_availability_zones.azs.names
+  public_subnets = var.public_subnets
 
-	enable_dns_hostnames = true
+  enable_dns_hostnames = true
 
-	tags = {
-		Name        = "jenkins-vpc"
-		Terraform   = "true"
-		Environment = "dev"
-	}
+  tags = {
+    Name        = "jenkins-vpc"
+    Terraform   = "true"
+    Environment = "dev"
+  }
 
-	public_subnet_tags = {
-		Name = "jenkins-subnet"
-	}
+  public_subnet_tags = {
+    Name = "jenkins-subnet"
+  }
 }
 
 # SG
+module "sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "jenkins-sg"
+  description = "Security Group for Jenkins Server."
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 8080
+      to_port     = 8080
+      protocol    = "tcp"
+      description = "HTTP"
+      cidr_blocks = "0.0.0.0/0"
+    },
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
 
 # EC2
