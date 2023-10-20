eb37cd3d8cdfce58909eda8b1451b097a7d61793
Milan Obrenovic <mmobrenovic@gmail.com>
2023-10-20 15:25:35 +0200

    Added jenkins-install.sh and configured ec2 instance

--- main.tf
+++ main.tf
@@ -81,3 +81,23 @@ module "sg" {
 }
 
 # EC2
+module "ec2_instance" {
+	source = "terraform-aws-modules/ec2-instance/aws"
+
+	name = "Jenkins-Server"
+
+	instance_type               = var.instance_type
+	key_name                    = "devops"
+	monitoring                  = true
+	vpc_security_group_ids      = [module.sg.security_group_id]
+	subnet_id                   = module.vpc.public_subnets[0]
+	associate_public_ip_address = true
+	user_data                   = file("jenkins-install.sh")
+	availability_zone           = data.aws_availability_zones.azs.names[0]
+
+	tags = {
+		Name        = "Jenkins-Server"
+		Terraform   = "true"
+		Environment = "dev"
+	}
+}
